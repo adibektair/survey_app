4a46cb324b40acd4a51184cdcde586785239a724
Tair Adibek <adibek.tair@gmail.com>
2022-03-05 06:57:33 +0300

    Initial Commit

---
+++ XM Test taskUITests/XM_Test_taskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  XM_Test_taskUITestsLaunchTests.swift
+//  XM Test taskUITests
+//
+//  Created by tair on 5.03.22.
+//
+
+import XCTest
+
+class XM_Test_taskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
